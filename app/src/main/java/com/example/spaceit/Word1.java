6a60ef9682e321be2403e8040782cacceff77ca4
Yaroslav22 <ioninyaroslav22@gmail.com>
2021-06-15 18:18:27 +0500

    SpaceIT App

---
+++ app/src/main/java/com/example/spaceit/Word1.java
@@ -0,0 +1,86 @@
+package com.example.spaceit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Word1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.word1);
+        Button button_back = findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Word1.this, WordsLevel.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+                }
+            }
+        });
+        Button button_next = findViewById(R.id.button_next);
+        button_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Word1.this, Word2.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+                }
+            }
+        });
+        Button button_variant1 = findViewById(R.id.variant1);
+        button_variant1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction()==MotionEvent.ACTION_DOWN){
+                    button_variant1.setBackgroundResource(R.drawable.style_button_incorrectly);
+                }
+                return false;
+            }
+        });
+        Button button_variant2 = findViewById(R.id.variant2);
+        button_variant2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction()==MotionEvent.ACTION_DOWN){
+                    button_variant2.setBackgroundResource(R.drawable.style_button_right);
+                }
+                return false;
+            }
+        });
+        Button button_variant3 = findViewById(R.id.variant3);
+        button_variant3.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction()==MotionEvent.ACTION_DOWN){
+                    button_variant3.setBackgroundResource(R.drawable.style_button_incorrectly);
+                }
+                return false;
+            }
+        });
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+    @Override
+    public void onBackPressed(){
+        try {
+            Intent intent = new Intent(Word1.this, WordsLevel.class);
+            startActivity(intent);
+            finish();
+        }catch (Exception e){
+        }
+    }
+
+}
--- app/src/main/java/com/example/spaceit/WordsLevel.java
+++ app/src/main/java/com/example/spaceit/WordsLevel.java
@@ -27,6 +27,30 @@ public class WordsLevel extends AppCompatActivity {
                 }
             }
         });
+        Button button_word1 = findViewById(R.id.buttonWord1);
+        button_word1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(WordsLevel.this, Word1.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+                }
+            }
+        });
+        Button button_word2 = findViewById(R.id.buttonWord2);
+        button_word2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(WordsLevel.this, Word2.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+                }
+            }
+        });
         Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
